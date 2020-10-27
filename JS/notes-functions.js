3132a71bccd4363ff0a9b86e94204bf61882ba87
VajihuddinAhmed <vajihahmed@gmail.com>
2020-10-27 21:28:01 +0100

    Home page finished

---
+++ JS/notes-functions.js
@@ -0,0 +1,132 @@
+'use strict'
+
+// Read existing notes from localStorage
+const getSavedNotes = () => {
+    const notesJSON = localStorage.getItem('notes')
+    try {
+        return notesJSON !== null ? JSON.parse(notesJSON) : []
+    } catch (e) {
+        return []
+    }
+}
+
+// Save the notes to localStorage
+const saveNotes = (notes) => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Remove note function setup
+const removeNote = (item, i) => {
+    const noteIndex = notes.findIndex((ItemNote) => ItemNote.id === item)
+
+        if (noteIndex > -1) {
+            notes.splice(noteIndex, 1)
+        }
+}
+
+// Generate the DOM structure for a note
+const generateNoteDOM = (note) => {
+    const noteEl = document.createElement('a')
+    const textEl = document.createElement('p')
+    const statusEL = document.createElement('p')
+    
+    // // Setup the remove note button    
+    // const button = document.createElement('button')
+
+    // button.textContent = 'x'
+    // noteEl.appendChild(button)
+    // button.addEventListener('click', () => {
+    //     removeNote(note.id)
+    //     saveNotes(notes)
+    //     renderNotes(notes, filters)
+    // })
+
+    // Setup the note title text
+    if (note.title.length > 0) {
+        textEl.textContent = note.title
+    } else {
+        textEl.textContent = 'Unnamed note'
+    }
+    textEl.classList.add('list-item__title')
+    noteEl.appendChild(textEl)
+
+    // Setup the link
+    noteEl.setAttribute('href', `/notes-app/edit.html#${note.id}`)
+    noteEl.classList.add('list-item')
+
+    // Setup the status message
+    statusEL.textContent = generateLastEdited(note.updatedAt)
+    statusEL.classList.add('list-item__subtitle')
+    noteEl.appendChild(statusEL)
+
+
+    return noteEl
+}
+
+// Sort your notes by one of three ways
+
+const sortNotes = (notes, sortBy) => {
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
+// Render application notes
+const renderNotes = (notes, filters) => {
+    const notesEl = document.querySelector('#notes')
+    notes = sortNotes(notes, filters.sortBy)
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    notesEl.innerHTML = ''
+
+    if (filteredNotes.length > 0) {
+        filteredNotes.forEach((note) => {
+            const noteEl = generateNoteDOM(note)
+            notesEl.appendChild(noteEl)
+        })
+    } else {
+        const emptyMessage = document.createElement('p')
+        emptyMessage.textContent = 'No notes to show'
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
+    }
+  
+}
+
+
+// Generate the last edited message
+const generateLastEdited = (timestamp) => {
+    return `Last edited ${moment(timestamp).fromNow()}`
+}
